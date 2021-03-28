02b6fd32e73148b54653da34506e42b8831552b5
Wes Rupert <rupertwe@amazon.com>
2021-03-27 22:18:27 -0700

    Add shared key segments

--- keyboards/hotdox/keymaps/ora/keymap.c
+++ keyboards/hotdox/keymaps/ora/keymap.c
@@ -16,25 +16,25 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_BASE] = LAYOUT_wrapper( // {{{
-    KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,    KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    MT_LCES, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, MT_RCEN,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,     KC_SCLN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LALT, MC_MPRV, TD_MPLS, MC_MNXT, KC_LGUI,                                        KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    KC_DEL,                  __QWER_L1__,                                             __QWER_R1__,        KC_LBRC, KC_RBRC, KC_BSPC,
+    KC_TAB,                  __QWER_L2__,                 KC_MINS,    KC_EQL,         __QWER_R2__,                          KC_BSLS,
+    MT_LCES,                 __QWER_L3__,                                             __QWER_R3__,                 KC_QUOT, MT_RCEN,
+    KC_LSFT,                 __QWER_L4__,                 KC_GRV,     KC_SCLN,        __QWER_R4__,                          KC_RSFT,
+    KC_LALT,        __MDIA_MC__,        KC_LGUI,                                        KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
                                                  LT_NMPD, TD_DMC1,    TD_DMC2, KC_LOCK,
                                         /*-----\ /-----*/ KC_LEAD,    KC_LEAD, /*-----\ /-----*/
-                                        MT_LSSP, LT_SYEN, LT_FNES,    LT_FNES, LT_SYEN, MT_RSSP)
-, // }}}
+                                        MT_LSSP, LT_SYEN, LT_FNES,    LT_FNES, LT_SYEN, MT_RSSP),
+// }}}
 [LAYER_WIN] = LAYOUT_wrapper( // {{{
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-    _______, WN_MPRV, TD_WPLS, WN_MNXT, _______,                                        _______, _______, _______, _______, _______,
+    _______,        __MDIA_WN__,        _______,                                        _______, _______, _______, _______, _______,
                                                  _______, _______,    _______, _______,
                                         /*-----\ /-----*/ _______,    _______, /*-----\ /-----*/
-                                        _______, _______, LT_WFNE,    LT_WFNE, _______, _______)
-, // }}}
+                                        _______, _______, LT_WFNE,    LT_WFNE, _______, _______),
+// }}}
 [LAYER_GAMES] = LAYOUT_wrapper( // {{{
     KC_GRV,  _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
@@ -43,48 +43,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                          _______, _______, _______, _______, _______,
                                                  WN_MPLY, WN_MNXT,    _______, _______,
                                         /*-----\ /-----*/ LT_WFNE,    _______, /*-----\ /-----*/
-                                        KC_SPC,  KC_LALT, KC_ESC,     _______, _______, _______)
-, // }}}
+                                        KC_SPC,  KC_LALT, KC_ESC,     _______, _______, _______),
+// }}}
 [LAYER_FUNCTION] = LAYOUT_wrapper( // {{{
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,
-    KC_CAPS, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  XXXXXXX,    XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  MC_PSCR, KC_CAPS,
-    KC_LCTL, XXXXXXX, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MC_PSCF, KC_RCTL,
+    KC_ESC,                  __FUNC_L1__,                                                      __FUNC_R1__,                 RESET,
+    KC_CAPS, XXXXXXX,        __FUNC_L2__,                 XXXXXXX,    XXXXXXX,                 __FUNC_R2__,        MC_PSCR, KC_CAPS,
+    KC_LCTL, XXXXXXX,        __FUNC_L3__,                                                      __FUNC_R3__,        MC_PSCF, KC_RCTL,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, MC_VOLD, MC_MUTE, MC_VOLU, _______,                                        MC_MPLY, MC_MPRV, MC_VOLD, MC_VOLU, MC_MNXT,
+    _______,        __VOLM_MC__,        _______,                                        TD_MPLS, MC_MPRV, MC_VOLD, MC_VOLU, MC_MNXT,
                                                  _______, _______,    _______, MC_LOCK,
                                         /*-----\ /-----*/ _______,    _______, /*-----\ /-----*/
-                                        _______, _______, _______,    _______, _______, _______)
-, // }}}
+                                        _______, _______, _______,    _______, _______, _______),
+// }}}
 [LAYER_WINFUN] = LAYOUT_wrapper( // {{{
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,
-    KC_CAPS, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  XXXXXXX,    XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  WN_PSCR, KC_CAPS,
-    KC_LCTL, XXXXXXX, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, WN_PSCF, KC_RCTL,
+    KC_ESC,                  __FUNC_L1__,                                                      __FUNC_R1__,                 RESET,
+    KC_CAPS, XXXXXXX,        __FUNC_L2__,                 XXXXXXX,    XXXXXXX,                 __FUNC_R2__,        MC_PSCR, KC_CAPS,
+    KC_LCTL, XXXXXXX,        __FUNC_L3__,                                                      __FUNC_R3__,        MC_PSCF, KC_RCTL,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, WN_VOLD, WN_MUTE, WN_VOLU, _______,                                        WN_MPLY, WN_MPRV, WN_VOLD, WN_VOLU, WN_MNXT,
+    _______,        __VOLM_WN__,        _______,                                        TD_WPLS, WN_MPRV, WN_VOLD, WN_VOLU, WN_MNXT,
                                                  _______, _______,    _______, WN_LOCK,
                                         /*-----\ /-----*/ _______,    _______, /*-----\ /-----*/
-                                        _______, _______, _______,    _______, _______, _______)
-, // }}}
+                                        _______, _______, _______,    _______, _______, _______),
+// }}}
 [LAYER_SYMBOLS] = LAYOUT_wrapper( // {{{
-    _______,                               __SYM1L__,                              __SYM1R__,                               _______,
-    _______,                  __SYM2L__,                  KC_UNDS,    KC_PLUS,                   __SYM2R__,                 _______,
-    _______,                  __SYM3L__,                                                         __SYM3R__,                 _______,
-    _______,                  __SYM4L__,                  KC_TILD,    KC_COLN,                   __SYM4R__,                 _______,
+    _______,                               __SYMB_L1__,                          __SYMB_R1__,                               _______,
+    _______,                 __SYMB_L2__,                 KC_UNDS,    KC_PLUS,                 __SYMB_R2__,                 _______,
+    _______,                 __SYMB_L3__,                                                      __SYMB_R3__,                 _______,
+    _______,                 __SYMB_L4__,                 KC_TILD,    KC_COLN,                 __SYMB_R4__,                 _______,
     _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
                                                  _______, _______,    _______, _______,
                                         /*-----\ /-----*/ _______,    _______, /*-----\ /-----*/
-                                        _______, _______, _______,    _______, _______, _______)
-, // }}}
+                                        _______, _______, _______,    _______, _______, _______),
+// }}}
 [LAYER_NUMPAD] = LAYOUT_wrapper( // {{{
-     KC_BSPC, KC_DEL,  XXXXXXX, KC_PSLS, KC_PSLS, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_PSLS, KC_PSLS, XXXXXXX, KC_BSPC, KC_DEL,
-     KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_PAST, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_NLCK,
-     _______, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, XXXXXXX,                      XXXXXXX, KC_PMNS, KC_P4,   KC_P5,   KC_P6,   _______,
-     _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_PPLS, KC_P1,   KC_P2,   KC_P3,   _______,
-     _______, XXXXXXX, KC_PDOT, KC_P0,   KC_PEQL,                                        KC_PEQL, KC_P0,   KC_PDOT, XXXXXXX, _______,
+     XXXXXXX,             __NMPD_L1__,            XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,            __NMPD_R1__,             XXXXXXX,
+     KC_NLCK,             __NMPD_L2__,            XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,            __NMPD_R2__,             KC_NLCK,
+     _______,             __NMPD_L3__,            XXXXXXX,                      XXXXXXX,            __NMPD_R3__,             _______,
+     _______,             __NMPD_L4__,            XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,            __NMPD_R4__,             _______,
+     _______,             __NMPD_L5__,            XXXXXXX,                      XXXXXXX,            __NMPD_R5__,             _______,
                                                   _______, _______,    _______, _______,
                                          /*-----\ /-----*/ _______,    _______, /*-----\ /-----*/
                                          KC_SPC,  KC_PENT, _______,    _______, KC_PENT, KC_SPC)
-//, // }}}
+// }}}
 //[LAYER_] = LAYOUT_wrapper( // {{{
 //    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
 //    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
@@ -93,8 +93,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
 //                                                 _______, _______,    _______, _______,
 //                                        /*-----\ /-----*/ _______,    _______, /*-----\ /-----*/
-//                                        _______, _______, _______,    _______, _______, _______)
-//, // }}}
+//                                        _______, _______, _______,    _______, _______, _______) COMMA???
+//// }}}
 };
 
 
--- keyboards/whitefox/keymaps/ora/keymap.c
+++ keyboards/whitefox/keymaps/ora/keymap.c
@@ -3,8 +3,10 @@
 #include "version.h"
 
 #define LT_FNSP LT(LAYER_FUNCTION, KC_SPC)
+#define LT_FNES LT(LAYER_FUNCTION, KC_ESC)
 #define LT_WFNS LT(LAYER_WINFUN, KC_SPC)
 #define LT_SYES LT(LAYER_SYMBOLS, KC_ESC)
+#define LT_SYBS LT(LAYER_SYMBOLS, KC_BSPC)
 #define MT_LCES LCTL_T(KC_ESC)
 #define MT_RCEN RCTL_T(KC_ENT)
 
@@ -13,17 +15,17 @@
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_BASE] = LAYOUT_wrapper( // {{{
-    KC_GRV,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,KC_LEAD,MC_LOCK,
-    KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,      TD_DMC1,
-    MT_LCES,      KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,__MIS__,  MT_RCEN,  KC_PGUP,
-    KC_LSFT,__MIS__,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSFT,     KC_UP,  KC_PGDN,
-    KC_LALT,KC_LGUI,LT_SYES,                        LT_FNSP,                        LT_SYES,KC_RGUI,__MIS__,  KC_LEFT,KC_DOWN,KC_RGHT),
+    KC_GRV,                        __QWER_L1__,                   __QWER_R1__,                KC_MINS,KC_EQL, KC_BSPC,KC_LEAD,TD_DMC1,
+    KC_TAB,                        __QWER_L2__,                   __QWER_R2__,                  KC_LBRC,KC_RBRC,KC_BSLS,      MC_MPLY,
+    MT_LCES,                       __QWER_L3__,                   __QWER_R3__,            KC_SCLN,KC_QUOT,__MIS__,  MT_RCEN,  MC_MNXT,
+    KC_LSFT,__MIS__,               __QWER_L4__,                   __QWER_R4__,                           KC_RSFT,     KC_UP,  MC_MPRV,
+    KC_LALT,KC_LGUI,LT_SYES,                       LT_FNSP,                        LT_SYBS,KC_RGUI,__MIS__,  KC_LEFT,KC_DOWN,KC_RGHT),
 // }}}
 [LAYER_WIN] = LAYOUT_wrapper( // {{{
     _______,  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,WN_LOCK,
-    _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,      _______,
-    _______,      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,__MIS__,  _______,  _______,
-    _______,__MIS__,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,     _______,_______,
+    _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,      WN_MPLY,
+    _______,      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,__MIS__,  _______,  WN_MNXT,
+    _______,__MIS__,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,     _______,WN_MPRV,
     _______,_______,_______,                        LT_WFNS,                        _______,_______,__MIS__,  _______,_______,_______),
 // }}}
 [LAYER_GAMES] = LAYOUT_wrapper( // {{{
@@ -31,35 +33,35 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,      _______,
     KC_LCTL,      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,__MIS__,  _______,  _______,
     KC_LSFT,__MIS__,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,     _______,_______,
-    KC_LALT,KC_LGUI,KC_ESC,                         _______,                        _______,_______,__MIS__,  _______,_______,_______),
+    KC_LALT,KC_LGUI,KC_ESC,                         KC_SPC,                         LT_FNES,_______,__MIS__,  _______,_______,_______),
 // }}}
 [LAYER_FUNCTION] = LAYOUT_wrapper( // {{{
-    KC_GRV,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, XXXXXXX,XXXXXXX,
-    KC_CAPS,    MC_MPRV,MC_MPLY,MC_MNXT,XXXXXXX,XXXXXXX,KC_HOME,KC_PGDN,KC_PGUP,KC_END, MC_PSCR,MC_PSCF,XXXXXXX,XXXXXXX,      TD_DMC2,
-    KC_CAPS,      MC_VOLD,MC_MUTE,MC_VOLU,XXXXXXX,XXXXXXX,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,XXXXXXX,XXXXXXX,__MIS__,  MC_MUTE,  KC_PGUP,
-    _______,__MIS__,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,     MC_MPLY,     MC_VOLU,KC_PGDN,
-    _______,_______,_______,                        _______,                        _______,_______,__MIS__,  MC_MPRV,MC_VOLD,MC_MNXT),
+    KC_GRV,                        __FUNC_L1__,                   __FUNC_R1__,                                KC_DEL, TD_DMC2,MC_LOCK,
+    _______,XXXXXXX,               __FUNC_L2__,                   __FUNC_R2__,          MC_PSCR,MC_PSCF,XXXXXXX,XXXXXXX,      MC_MUTE,
+    KC_CAPS,XXXXXXX,               __FUNC_L3__,                   __FUNC_R3__,            XXXXXXX,XXXXXXX,__MIS__,  _______,  MC_VOLU,
+    _______,__MIS__,     __VOLM_MC__,       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,      __VOLM_MC__,           _______,     KC_PGUP,MC_VOLD,
+    _______,_______,_______,                        _______,                        _______,_______,__MIS__,  KC_HOME,KC_PGDN,KC_END),
 // }}}
 [LAYER_WINFUN] = LAYOUT_wrapper( // {{{
-    KC_GRV,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, XXXXXXX,XXXXXXX,
-    KC_CAPS,    WN_MPRV,WN_MPLY,WN_MNXT,XXXXXXX,XXXXXXX,KC_HOME,KC_PGDN,KC_PGUP,KC_END, WN_PSCR,WN_PSCF,XXXXXXX,XXXXXXX,      TD_DMC2,
-    KC_CAPS,      WN_VOLD,WN_MUTE,WN_VOLU,XXXXXXX,XXXXXXX,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,XXXXXXX,XXXXXXX,__MIS__,  WN_MUTE,  KC_PGUP,
-    _______,__MIS__,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,     WN_MPLY,     WN_VOLU,KC_PGDN,
-    _______,_______,_______,                        _______,                        _______,_______,__MIS__,  WN_MPRV,WN_VOLD,WN_MNXT),
+    KC_GRV,                        __FUNC_L1__,                   __FUNC_R1__,                                KC_DEL, TD_DMC2,WN_LOCK,
+    _______,XXXXXXX,               __FUNC_L2__,                   __FUNC_R2__,          WN_PSCR,WN_PSCF,XXXXXXX,XXXXXXX,      WN_MUTE,
+    KC_CAPS,XXXXXXX,               __FUNC_L3__,                   __FUNC_R3__,            XXXXXXX,XXXXXXX,__MIS__,  _______,  WN_VOLU,
+    _______,__MIS__,     __VOLM_WN__,       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,      __VOLM_WN__,           _______,     KC_PGUP,WN_VOLD,
+    _______,_______,_______,                        _______,                        _______,_______,__MIS__,  KC_HOME,KC_PGDN,KC_END),
 // }}}
 [LAYER_SYMBOLS] = LAYOUT_wrapper( // {{{
-    KC_TILD,                           __SYM1L__,                           __SYM1R__,                        _______,_______,_______,
-    _______,                        __SYM2L__,                    __SYM2R__,                    KC_LPRN,KC_RPRN,_______,      _______,
-    _______,                          __SYM3L__,                    __SYM3R__,                    KC_DQUO,__MIS__,  _______,  _______,
-    _______,__MIS__,                    __SYM4L__,                     __SYM4R__,                        _______,     _______,_______,
+    KC_TILD,                       __SYMB_L1__,                   __SYMB_R1__,                                _______,_______,_______,
+    _______,                       __SYMB_L2__,                   __SYMB_R2__,                  KC_LPRN,KC_RPRN,_______,      _______,
+    _______,                       __SYMB_L3__,                   __SYMB_R3__,                    KC_DQUO,__MIS__,  _______,  _______,
+    _______,__MIS__,               __SYMB_L4__,                   __SYMB_R4__,                           _______,     _______,_______,
     _______,_______,_______,                        _______,                        _______,_______,__MIS__,  _______,_______,_______),
 // }}}
 [LAYER_NUMPAD] = LAYOUT_wrapper( // {{{
-    KC_NLCK,  KC_PMNS,KC_PPLS,KC_PSLS,KC_PAST,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
-    KC_BSPC,    KC_P7,  KC_P8,  KC_P9,  KC_PEQL,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,      XXXXXXX,
-    _______,      KC_P4,  KC_P5,  KC_P6,  KC_PCMM,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,__MIS__,  _______,  _______,
-    _______,__MIS__,KC_P1,  KC_P2,  KC_P3,  KC_PDOT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,     _______,     _______,_______,
-    _______,_______,KC_P0,                          KC_PENT,                        _______,_______,__MIS__,  _______,_______,_______),
+    KC_NLCK,           __NMPD_L1__,           XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
+    KC_BSPC,           __NMPD_L2__,             XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,      XXXXXXX,
+    _______,           __NMPD_L3__,               XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,__MIS__,  _______,  _______,
+    _______,__MIS__,   __NMPD_L4__,                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,     _______,     _______,_______,
+    _______,           __NMPD_L5__,                                                 KC_P0,  KC_PDOT,__MIS__,  _______,_______,_______),
 // }}}
 /* [LAYER_TYPE] = LAYOUT_wrapper( // {{{
     _______,  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
