60ab533afb6852a1f01ac40e2d0b976976780267
Wes Rupert <wrupert@logitech.com>
2024-03-22 12:44:07 -0700

    update qmk_firmware

--- keyboards/hotdox/keymaps/ora/keymap.c
+++ keyboards/hotdox/keymaps/ora/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2023 Wes Rupert (@wesrupert)
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include "ora.h"
+#include "g/keymap_combo.h"
 
 enum layers {
     LAYER_NUMPAD = BASE_LAYER_ENUM_END,
@@ -45,8 +46,26 @@ enum tap_dances {
 // TODO: Investigate "Instant tap" mod taps for home-row mods:
 // https://github.com/filterpaper/qmk_userspace/blob/11cb3970e89760039b940f5de84e31c08aea4b68/readme.md?#contextual-mod-taps
 
-// Expand key groups before passing into layout macro.
+#define LAYOUT_ergodox_pretty(                                                 \
+      L00,L01,L02,L03,L04,L05,L06,              R00,R01,R02,R03,R04,R05,R06,   \
+      L10,L11,L12,L13,L14,L15,L16,              R10,R11,R12,R13,R14,R15,R16,   \
+      L20,L21,L22,L23,L24,L25,                      R21,R22,R23,R24,R25,R26,   \
+      L30,L31,L32,L33,L34,L35,L36,              R30,R31,R32,R33,R34,R35,R36,   \
+      L40,L41,L42,L43,L44,                              R42,R43,R44,R45,R46,   \
+                              L55,L56,      R50,R51,                           \
+                                  L54,      R52,                               \
+                          L51,L52,L53,      R53,R54,R55 )                      \
+   {                         /* matrix positions */                            \
+    { L00,L01,L02,L03,L04,L05,L06,              R00,R01,R02,R03,R04,R05,R06 }, \
+    { L10,L11,L12,L13,L14,L15,L16,              R10,R11,R12,R13,R14,R15,R16 }, \
+    { L20,L21,L22,L23,L24,L25,KC_NO,          KC_NO,R21,R22,R23,R24,R25,R26 }, \
+    { L30,L31,L32,L33,L34,L35,L36,              R30,R31,R32,R33,R34,R35,R36 }, \
+    { L40,L41,L42,L43,L44,KC_NO,KC_NO,      KC_NO,KC_NO,R42,R43,R44,R45,R46 }, \
+    { KC_NO,L51,L52,L53,L54,L55,L56,          R50,R51,R52,R53,R54,R55,KC_NO }  \
+   }
 #define L(...) LAYOUT_ergodox_pretty(__VA_ARGS__)
+
+// Expand key groups before passing into layout macro.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_BASE] = L(
     KC_DEL,  __________________QWER_L1__________________, KC_LBRC,    KC_RBRC, __________________QWER_R1__________________, KC_BSPC,
